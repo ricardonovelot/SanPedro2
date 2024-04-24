3999c5cf122a7475777086e992d04c64ab978a8d
ricardonovelot <ricardonovelo97.tools@gmail.com>
2024-04-24 13:41:07 -0600

    Initial Commit

---
+++ SanPedro/SanPedroApp.swift
@@ -0,0 +1,17 @@
+//
+//  SanPedroApp.swift
+//  SanPedro
+//
+//  Created by Ricardo on 24/04/24.
+//
+
+import SwiftUI
+
+@main
+struct SanPedroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
